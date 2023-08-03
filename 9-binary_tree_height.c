b232f13f0a805f38c9d7b796657cf511a50e3cfc
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-08-02 23:48:23 -0700

    updates

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,11 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l_height, r_height;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
 	if (!tree)
-		return (-1);
-	l_height = 1 + binary_tree_height(tree->left);
-	r_height = 1 + binary_tree_height(tree->right);
-	return (l_height > r_height ? l_height : r_height);
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
