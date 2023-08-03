7ba7f3ca565acc9110c3effd7bc80c2087e4705c
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-08-03 14:28:55 -0700

    18-binary_tree_uncle.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,6 +7,7 @@
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
+{
 	size_t height;
 	size_t nodes;
 	size_t power;
@@ -40,4 +41,38 @@ int _pow_recursion(int x, int y)
 	else
 		return (x * _pow_recursion(x, y - 1));
 
+}
+
+/**
+ * binary_tree_height -  measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: tree is NULL, your function must return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: If tree is NULL 0 else size of tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t x;
+
+	if (!tree)
+		return (0);
+	x = (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	return (x);
 }
